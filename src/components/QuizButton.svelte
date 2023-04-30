bd96812ca47329421ffc76dd44f724a1a65a52aa
maksimkadocnikov <maxim.kadocnikov@scorum.com>
2023-04-30 17:38:51 +0700

    updates

---
+++ src/components/QuizButton.svelte
@@ -0,0 +1,24 @@
+<!-- QuizButton.svelte -->
+<script>
+    export let title;
+    export let description;
+
+    let isOpen = false;
+
+    function toggleDescription() {
+        isOpen = !isOpen;
+    }
+</script>
+
+<div class="bg-secondary md:px-5 px-4 py-4 flex flex-col" on:click={toggleDescription}>
+    <div class="flex flex-row items-center justify-between">
+        <p class="md:text-3xl text-xl">{title}</p>
+        <img src={isOpen ? "/arrow-up.svg" : "/arrow-down.svg"} alt="arrow" class="md:w-4 md:h-4 h-3 w-3"/>
+    </div>
+    {#if isOpen}
+        <div class="md:text-2xl mt-10">
+            {description}
+        </div>
+    {/if}
+</div>
+
