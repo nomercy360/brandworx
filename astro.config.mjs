927412fde8f050d06e398e965d63f3df2adeb85b
maksimkadocnikov <maxim.kadocnikov@scorum.com>
2023-05-01 18:24:22 +0700

    robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  site: 'https://disrapt.co',
+  integrations: [tailwind(), svelte(), robotsTxt()]
 });
