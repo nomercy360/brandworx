04739baa0d4e5f10e2745329a32ad25211930e18
maksimkadocnikov <maxim.kadocnikov@scorum.com>
2023-05-01 17:41:37 +0700

    added images

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,35 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 
-	theme: {
-		extend: {
-			backgroundColor: {
-				'primary': '#000000',
-				'secondary': '#FFFFFF',
-				'gradient-start': '#6288da',
-				'gradient-end': '#e39999',
-				'green': '#38FE4C',
-				'gray': '#5A5A5A',
-				'yellow': '#FEF038',
-				'pink': '#D012A6',
-				'orange': '#FB5521',
-				'whispering-breeze': '#F5FDE5',
-			},
-			textColor: {
-				'primary': '#000000',
-				'secondary': '#FFFFFF',
-			},
-			borderColor: {
-				'secondary': '#FFFFFF',
-			},
-			colors: {
-				'primary': '#000000',
-				'secondary': '#FFFFFF',
-			}
-		},
-	},
-	variants: {},
-	plugins: [],
+    theme: {
+        extend: {
+            backgroundColor: {
+                'primary': '#000000',
+                'secondary': '#FFFFFF',
+                'gradient-start': '#6288da',
+                'gradient-end': '#e39999',
+                'green': '#38FE4C',
+                'gray': '#5A5A5A',
+                'yellow': '#FEF038',
+                'pink': '#D012A6',
+                'orange': '#FB5521',
+                'whispering-breeze': '#F5FDE5',
+                'light-black': '#202020',
+                'red': '#DD1F1F',
+                'light-gray': '#EBEFEE',
+                'blue': '#3478F6',
+                'steel-blue': '#91A5BA',
+            },
+            textColor: {
+                'primary': '#000000',
+                'secondary': '#FFFFFF',
+            },
+            borderColor: {
+                'secondary': '#FFFFFF',
+            },
+            colors: {
+                'primary': '#000000',
+                'secondary': '#FFFFFF',
+            }
+        },
+    },
+    variants: {},
+    plugins: [],
 }
 
