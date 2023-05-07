73d29bb992a0d21e911411dd75985e70e2f73221
nomercy360 <maximkadocnikov@gmail.com>
2023-05-07 09:06:45 +0700

    site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import robotsTxt from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://disrapt.co',
-  integrations: [tailwind(), svelte(), robotsTxt()]
+    site: 'https://disrapt.co',
+    integrations: [tailwind(), svelte(), robotsTxt(), sitemap({
+        filter: (page) => page !== 'https://disrapt.co/admin/',
+    })],
 });
